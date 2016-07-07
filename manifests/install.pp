1000196ce38139ab6bc013e7ed50a7e311ac6f51
Addi Abel <addi.abel@digital.landregistry.gov.uk>
2016-07-07 13:32:59 +0100

    latest selinux patches blocked rabbitadmin ...

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,46 +10,53 @@
 # ===========================
 #
 class rabbit::install (
-  $ensure               = $rabbit::params::ensure,
-  $version              = $rabbit::params::version,
-  $patch                = $rabbit::params::patch,
-  $package_name         = $rabbit::params::package_name
+  $ensure                  = $rabbit::params::ensure,
+  $version                 = $rabbit::params::version,
+  $patch                   = $rabbit::params::patch,
+  $ssl_management_port     = $rabbit::params::ssl_management_port,
+  $package_name            = $rabbit::params::package_name
 ) inherits rabbit::params {
 
   notify { "## --->>> Installing package: ${package_name}-${version}${patch}": }
 
   package { "${package_name}-${version}${patch}" :
-    ensure              => 'installed',
-    provider            => 'rpm',
-    source              => "https://www.rabbitmq.com/releases/${package_name}/v${version}/${package_name}-${version}${patch}.noarch.rpm",
+    ensure                 => 'installed',
+    provider               => 'rpm',
+    source                 => "https://www.rabbitmq.com/releases/${package_name}/v${version}/${package_name}-${version}${patch}.noarch.rpm",
   }
 
   selinux::module { 'rabbitmq':
-    ensure              => 'present',
-    source              => 'puppet:///modules/rabbit/rabbitmq.te'
+    ensure                 => 'present',
+    source                 => 'puppet:///modules/rabbit/rabbitmq.te'
   }
+
+  selinux::port { 'allow_rabbitadmin_port':
+    context                => 'rabbitmq_t',
+    port                   => $ssl_management_port,
+    protocol               => 'tcp',
+    }
 
   notify { "## --->>> removing old sysvinit file instlled by: ${package_name}": }
 
   exec { 'stop old init script':
-    command             => "systemctl disable ${package_name}",
-    path                => "/sbin:/bin:/usr/sbin:/usr/bin",
-    onlyif              => "test -x /etc/rc.d/init.d/${package_name}",
+    command                => "systemctl disable ${package_name}",
+    path                   => "/sbin:/bin:/usr/sbin:/usr/bin",
+    onlyif                 => "test -x /etc/rc.d/init.d/${package_name}",
   }
 
   exec { 'remove old init file':
-    command             => "rm -f /etc/rc.d/init.d/${package_name}",
-    path                => "/sbin:/bin:/usr/sbin:/usr/bin",
-    onlyif              => "test -x /etc/rc.d/init.d/${package_name}",
+    command                => "rm -f /etc/rc.d/init.d/${package_name}",
+    path                   => "/sbin:/bin:/usr/sbin:/usr/bin",
+    onlyif                 => "test -x /etc/rc.d/init.d/${package_name}",
   }
 
   # vmware installs have capital hostnames ... YUK !!! - provide a clean hosts file
   file { '/etc/hosts.fix':
-    ensure              => 'present',
-    owner               => 'root',
-    group               => 'root',
-    mode                => '0644',
-    source              => "puppet:///modules/rabbit/hosts",
+    ensure                 => 'present',
+    owner                  => 'root',
+    group                  => 'root',
+    mode                   => '0644',
+    source                 => "puppet:///modules/rabbit/hosts",
   }
 
 }
