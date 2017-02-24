f05ebfff03767ebe7c52597ef9641475592120d5
Addi Abel <addi@abel.de.com>
2017-02-24 17:41:50 +0000

    typo in manifest for accounts

--- manifests/account.pp
+++ manifests/account.pp
@@ -18,7 +18,7 @@ class rabbit::account (
   $home_dir     = $rabbit::params::home_dir
   ) {
 
-  incluse rabbit::params
+  include rabbit::params
 
 # notify { "## --->>> Creating accounts for: ${package_name}": }
 
